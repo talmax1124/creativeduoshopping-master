21a6c5b06f254ba88508a4baa01b616f9ba463c8
talmax1124 <carlitosdiazplaza@gmail.com>
2021-06-18 16:41:24 -0400

    Stock badge

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,25 +1,21 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { Card } from "react-bootstrap";
+import { Card, Badge } from "react-bootstrap";
 import Rating from "./Rating";
-import Favorites from './Favorites'
+import Favorites from "./Favorites";
 
 const Product = ({ product }) => {
-
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
 
   return (
-  
     <Card className="my-3 p-1 rounded cardsss">
       <Link to={`/product/${product._id}`}>
         <Card.Img src={product.image} variant="top" className="imggg" />
       </Link>
 
-    {userInfo && (
-              <Favorites product={product} />
-    )}
+      {userInfo && <Favorites product={product} />}
 
       <Card.Body>
         <Link to={`/product/${product._id}`}>
@@ -27,6 +23,18 @@ const Product = ({ product }) => {
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
+
+        <Card.Text as="p">
+          {product.countInStock > 0 ? (
+            <Badge  bg="success" variant="success">
+              In Stock
+            </Badge>
+          ) : (
+            <Badge  bg="danger" variant="danger">
+              Out Of Stock
+            </Badge>
+          )}
+        </Card.Text>
 
         <Card.Text as="div">
           <Rating
