e8feffce4e1afb8ee187c1e297cb91fe2ce923fa
talmax1124 <carlitosdiazplaza@gmail.com>
2021-06-30 23:35:44 -0400

    Changed Categories Options Again

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -353,7 +353,8 @@ const ProductEditScreen = ({ match, history }) => {
                 <option value="...">Select...</option>
                 <option value="Shirts">Shirts</option>
                 <option value="Infused Design">Infused Design</option>
-                <option value="Cups/Tumblers/Mugs">Cups/Tumblers/Mugs</option>
+                <option value="Mug / Cup">Mug/Cup</option>
+                <option value="Cups-Tumblers-Mugs">Cups/Tumblers/Mugs</option>
                 <option value="Stickers">Stickers</option>
                 <option value="Car Products">Car Products</option>
                 <option value="Other">Other</option>
