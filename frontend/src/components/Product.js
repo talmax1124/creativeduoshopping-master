9162e4422d9edce4d1a89ec90f2aaa8531febded
talmax1124 <carlitosdiazplaza@gmail.com>
2021-07-22 02:27:55 -0400

    Added Show More Details Button

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { Card, Badge } from "react-bootstrap";
+import { Card, Badge, Toast, Button } from "react-bootstrap";
 import Rating from "./Rating";
 import Favorites from "./Favorites";
 
 const Product = ({ product }) => {
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo } = userLogin;
+
+  const [show, setShow] = useState(false);
 
   return (
     <Card className="my-3 p-1 rounded cardsss">
@@ -26,11 +28,11 @@ const Product = ({ product }) => {
 
         <Card.Text as="p">
           {product.countInStock > 0 ? (
-            <Badge  bg="success" variant="success">
+            <Badge bg="success" variant="success">
               In Stock
             </Badge>
           ) : (
-            <Badge  bg="danger" variant="danger">
+            <Badge bg="danger" variant="danger">
               Out Of Stock
             </Badge>
           )}
@@ -39,14 +41,18 @@ const Product = ({ product }) => {
         <Card.Text as="div">
           <Rating
             value={product.rating}
-            text= {product.numReviews > 1 ? (`${product.numReviews} review(s)`) : (`${product.numReviews} review`)}
+            text={
+              product.numReviews > 1
+                ? `${product.numReviews} review(s)`
+                : `${product.numReviews} review`
+            }
             // {`${product.numReviews} reviews`}
           />
         </Card.Text>
 
         {product.lastPrice > 0 && (
           <>
-          <br/>
+            <br />
             <Card.Text as="h5">Original Price: ${product.lastPrice}</Card.Text>
           </>
         )}
@@ -64,6 +70,23 @@ const Product = ({ product }) => {
             </Card.Text>
           </>
         )}
+        <Button onClick={() => setShow(true)} className="btn btn-block moredeets">More Details</Button>
+
+        <Toast
+          onClose={() => setShow(false)}
+          show={show}
+          delay={10000}
+          autohide
+          style={{marginTop: "15px"}}
+        >
+          <Toast.Header>
+            <strong className="me-auto">More Info On Product</strong>
+          </Toast.Header>
+          <Toast.Body>
+          Brand: {product.brand} <br />
+          Category: {product.category}
+          </Toast.Body>
+        </Toast>
       </Card.Body>
     </Card>
   );
